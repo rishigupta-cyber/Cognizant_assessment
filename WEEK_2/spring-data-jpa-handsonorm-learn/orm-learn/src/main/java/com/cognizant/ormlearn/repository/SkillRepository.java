88dac084f4d62d80c860da6f240f5f137588fcec
Rishi Gupta <rishiguptaorai04@gmail.com>
2026-07-05 03:31:17 +0530

    Delete WEEK_2 directory

--- WEEK_2/spring-data-jpa-handsonorm-learn/orm-learn/src/main/java/com/cognizant/ormlearn/repository/SkillRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cognizant.ormlearn.repository;
-import com.cognizant.ormlearn.model.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository public interface SkillRepository extends JpaRepository<Skill, Integer> {}
