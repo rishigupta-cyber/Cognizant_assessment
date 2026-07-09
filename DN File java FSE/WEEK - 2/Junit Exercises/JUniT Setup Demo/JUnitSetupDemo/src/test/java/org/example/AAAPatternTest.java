407795c4a1ad2a44a315dfe1d1dab4ab784cc9b9
rishigupta-cyber <rishiguptaorai04@gmail.com>
2026-07-09 15:36:27 +0530

    Add WEEK - 3

--- DN File java FSE/WEEK - 2/Junit Exercises/JUniT Setup Demo/JUnitSetupDemo/src/test/java/org/example/AAAPatternTest.java
+++ DN File java FSE/WEEK - 2/Junit Exercises/JUniT Setup Demo/JUnitSetupDemo/src/test/java/org/example/AAAPatternTest.java
@@ -1,4 +1,52 @@
 package org.example;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class AAAPatternTest {
+
+    private int a;
+    private int b;
+
+    @Before
+    public void setUp() {
+        // Runs before every test method
+        System.out.println("Setup: initializing values before test...");
+        a = 10;
+        b = 5;
+    }
+
+    @Test
+    public void testAddition() {
+        // Arrange
+        int expected = 15;
+
+        // Act
+        int actual = a + b;
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubtraction() {
+        // Arrange
+        int expected = 5;
+
+        // Act
+        int actual = a - b;
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @After
+    public void tearDown() {
+        // Runs after every test method
+        System.out.println("Teardown: cleaning up after test...");
+        a = 0;
+        b = 0;
+    }
 }
