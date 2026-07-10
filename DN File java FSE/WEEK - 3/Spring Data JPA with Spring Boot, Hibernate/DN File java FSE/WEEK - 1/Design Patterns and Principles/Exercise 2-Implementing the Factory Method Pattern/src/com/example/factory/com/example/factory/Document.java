45e15987a0489d2fd65440032fb9ceee09a3dab9
rishigupta-cyber <rishiguptaorai04@gmail.com>
2026-07-10 23:56:08 +0530

    Fix: clean up Spring Data JPA folder structure

--- DN File java FSE/WEEK - 3/Spring Data JPA with Spring Boot, Hibernate/DN File java FSE/WEEK - 1/Design Patterns and Principles/Exercise 2-Implementing the Factory Method Pattern/src/com/example/factory/com/example/factory/Document.java
+++
@@ -1,7 +0,0 @@
-package com.example.factory;
-
-// common interface that every document type must follow
-public interface Document {
-    void open();
-    void save();
-}
--- DN File java FSE/WEEK - 3/Spring Data JPA with Spring Boot, Hibernate/DN File java FSE/WEEK - 3/Spring Core and Maven/Exercise 2 Implementing Dependency Injection/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-
-    public String getBookById(int id) {
-        return "Book with ID " + id + ": The Great Gatsby";
-    }
-}
