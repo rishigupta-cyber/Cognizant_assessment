172df9d70490661eef2e0df4c9895b01981dc6a7
rishigupta-cyber <rishiguptaorai04@gmail.com>
2026-07-12 19:58:22 +0530

    Add remaining WEEK-4 exercises

--- DN File java FSE/WEEK - 4/Spring REST using Spring Boot 3/REST - Get country based on country code/spring-learn/src/main/java/controller/CountryController.java
+++ DN File java FSE/WEEK - 4/Spring REST using Spring Boot 3/REST - Get country based on country code/spring-learn/src/main/java/controller/CountryController.java
@@ -1,10 +1,14 @@
 package com.cognizant.spring_learn.controller;
 
 import com.cognizant.spring_learn.Country;
+import com.cognizant.spring_learn.service.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CountryController {
 
     private static final Logger logger = LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
 
     @RequestMapping("/country")
     public Country getCountryIndia() {
@@ -23,5 +30,16 @@ public class CountryController {
         logger.info("End of getCountryIndia() method");
 
         return india;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        logger.info("Start of getCountry() controller method");
+
+        Country result = countryService.getCountry(code);
+
+        logger.info("End of getCountry() controller method");
+
+        return result;
     }
 }
--- DN File java FSE/WEEK - 4/Spring REST using Spring Boot 3/REST - Get country based on country code/spring-learn/src/main/java/service/CountryService.java
+++ DN File java FSE/WEEK - 4/Spring REST using Spring Boot 3/REST - Get country based on country code/spring-learn/src/main/java/service/CountryService.java
@@ -1,4 +1,34 @@
-package service;
+package com.cognizant.spring_learn.service;
 
+import com.cognizant.spring_learn.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CountryService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CountryService.class);
+
+    public Country getCountry(String code) {
+        logger.info("Start of getCountry() service method");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+        @SuppressWarnings("unchecked")
+        List<Country> countryList = (List<Country>) context.getBean("countryList");
+
+        Country result = countryList.stream()
+                .filter(country -> country.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+
+        logger.info("End of getCountry() service method");
+
+        return result;
+    }
 }
