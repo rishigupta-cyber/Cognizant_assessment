c0b9ae3a38c91ece9f6a0f4a07e1f0ed756143bd
rishigupta-cyber <rishiguptaorai04@gmail.com>
2026-07-15 01:18:20 +0530

    Add authentication service that returns JWT

---
+++ DN File java FSE/WEEK - 4/Spring REST using Spring Boot 3/Create authentication service that returns JWT/src/main/java/com/cognizant/jwt/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.cognizant.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRATION_TIME = 20 * 60 * 1000; // 20 minutes
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+}
